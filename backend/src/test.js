e219e7c51d52499c4b32688eeab1929de0a7309d
Rithesh <asanthularithesh@gmail.com>
2025-08-07 00:52:54 +0530

    corrected

--- backend/src/test.js
+++ backend/src/test.js
@@ -1,12 +1,12 @@
-import  OpenAI  from "openai";
+import OpenAI from "openai";
 
 const client = new OpenAI({
-	baseURL: "https://router.huggingface.co/v1",
-	apiKey: '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',
+    baseURL: "https://router.huggingface.co/v1",
+    apiKey: process.env.API_KEY, // Use environment variable for secret key
 });
 
 const chatCompletion = await client.chat.completions.create({
-	model: "openai/gpt-oss-120b:novita",
+    model: "openai/gpt-oss-120b:novita",
     messages: [
         {
             role: "user",
