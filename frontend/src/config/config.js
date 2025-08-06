0c515e3214864962c1147dd1a728e7a69f1eaf4c
Rithesh <asanthularithesh@gmail.com>
2025-08-07 01:48:08 +0530

    confit

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,6 +1,5 @@
 const config={
     flaskUrl:import.meta.env.VITE_FLASK_URL,
-    backendUrl:'https://workout-5g40.onrender.com/api/v1/user',
-    // backendUrl:'http://localhost:4000/api/v1/user',
+    backendUrl:'https://workout-backend-oo7f.onrender.com/api/v1/user',
 }
-export  default config
+export  default config  
