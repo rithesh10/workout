2c3a13b90cf9eaff97c7d28d5da0ca9cd30506af
Rithesh <asanthularithesh@gmail.com>
2025-08-07 11:26:56 +0530

    added url

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,5 +1,5 @@
 const config={
     flaskUrl:import.meta.env.VITE_FLASK_URL,
-    backendUrl:'https://workout-fs4z.onrender.com/api/v1/user',
+    backendUrl:'https://workout-backend-oo7f.onrender.com/api/v1/user',
 }
 export  default config  
